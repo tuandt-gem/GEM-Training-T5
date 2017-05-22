101de3f668daa74099f33b6a1f87082618c86930
cakiem626688 <linh626688@gmail.com>
2017-05-22 15:59:24 +0700

    Add Chat Sample with FireBase

---
+++ ChaApp/app/src/main/java/com/linhdt/helix/chaapp/ChatMessage.java
@@ -0,0 +1,43 @@
+package com.linhdt.helix.chaapp;
+
+import java.util.Date;
+
+/**
+ * Created by DangThanhLinh on 22/05/2017.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+        messageTime = new Date().getTime();
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
